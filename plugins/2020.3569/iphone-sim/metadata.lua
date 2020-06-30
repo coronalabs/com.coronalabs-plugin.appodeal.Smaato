3a128406969f31fcf7179947f322b42700200da2
Vlad Shcherban <vlad@coronalabs.com>
2020-06-30 10:57:48 +0300

    Updating binaries

---
+++ plugins/2020.3569/iphone-sim/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDSmaatoAdapter', },
+		frameworks = { 'OMSDK_Smaato', 'SmaatoSDKBanner', 'SmaatoSDKCore', 'SmaatoSDKInterstitial', 'SmaatoSDKOpenMeasurement', 'SmaatoSDKRewardedAds', 'SmaatoSDKRichMedia', 'SmaatoSDKVideo', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
