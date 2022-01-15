9f3c53f507af9ecb9b1c39f07043595b97b4337d
Scott Harrison <scottrules44@gmail.com>
2022-01-15 11:32:43 -0500

    not currently by supported by Appodeal replacing with stub

--- plugins/2018.3326/iphone-sim/metadata.lua
+++ plugins/2018.3326/iphone-sim/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'APDSmaatoAdapter', },
-		frameworks = { 'OMSDK_Smaato', 'SmaatoSDKBanner', 'SmaatoSDKCore', 'SmaatoSDKInterstitial', 'SmaatoSDKNative', 'SmaatoSDKOpenMeasurement', 'SmaatoSDKRewardedAds', 'SmaatoSDKRichMedia', 'SmaatoSDKVideo', },
+		staticLibs = { },
+		frameworks = { },
 		frameworksOptional = {},
 	},
 }
